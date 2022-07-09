e7f72317e92a3c8bc79195bebd3d8957451c30a3
Dennis Denk <dennis.denk@reichertshausen.net>
2022-07-09 20:30:58 +0200

    Users Endpoint created, not yet finished. Recreate initial commit, checking if aspnetusers table is still being created

---
+++ src/Application/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace OpenExam.Application.Users.Commands.CreateUser;
+
+public class CreateTodoItemCommandValidator : AbstractValidator<CreateTodoItemCommand>
+{
+    public CreateTodoItemCommandValidator()
+    {
+        RuleFor(v => v.Title)
+            .MaximumLength(200)
+            .NotEmpty();
+    }
+}
---
+++ src/Application/Users/Commands/UpdateUser/UpdateUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace OpenExam.Application.Users.Commands.UpdateUser;
+
+public class UpdateTodoItemCommandValidator : AbstractValidator<UpdateTodoItemCommand>
+{
+    public UpdateTodoItemCommandValidator()
+    {
+        RuleFor(v => v.Title)
+            .MaximumLength(200)
+            .NotEmpty();
+    }
+}
