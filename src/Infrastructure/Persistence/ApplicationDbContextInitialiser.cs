de24638723a740ab998b30f37b02f5c00fd9655d
Dennis Denk <dennis.denk@reichertshausen.net>
2022-09-29 21:42:55 +0200

    keycloak start

--- src/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
+++ src/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
@@ -11,15 +11,17 @@ public class ApplicationDbContextInitialiser
 {
     private readonly ILogger<ApplicationDbContextInitialiser> _logger;
     private readonly ApplicationDbContext _context;
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly RoleManager<IdentityRole> _roleManager;
+    // private readonly UserManager<ApplicationUser> _userManager;
+    // private readonly RoleManager<IdentityRole> _roleManager;
 
-    public ApplicationDbContextInitialiser(ILogger<ApplicationDbContextInitialiser> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    public ApplicationDbContextInitialiser(ILogger<ApplicationDbContextInitialiser> logger, ApplicationDbContext context
+        // , UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager
+        )
     {
         _logger = logger;
         _context = context;
-        _userManager = userManager;
-        _roleManager = roleManager;
+        // _userManager = userManager;
+        // _roleManager = roleManager;
     }
 
     public async Task InitialiseAsync()
@@ -57,12 +59,12 @@ public class ApplicationDbContextInitialiser
     public async Task TrySeedAsync()
     {
         // Default roles
-        var administratorRole = new IdentityRole("Administrator");
+        /* var administratorRole = new IdentityRole("Administrator");
 
         if (_roleManager.Roles.All(r => r.Name != administratorRole.Name))
         {
             await _roleManager.CreateAsync(administratorRole);
-        }
+        } */
 
         // Default users
         // var administrator = new ApplicationUser { UserName = "administrator@localhost", Email = "administrator@localhost" };
